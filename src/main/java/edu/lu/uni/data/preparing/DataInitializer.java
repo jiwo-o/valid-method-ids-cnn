8412f87bf7abf3cf2c3558d0955b98992a27b8af
Kui LIU <brucekuiliu@gmail.com>
2016-12-16 16:54:27 +0100

    Deep learning on data with CNN.

--- src/main/java/edu/lu/uni/data/preparing/DataInitializer.java
+++ src/main/java/edu/lu/uni/data/preparing/DataInitializer.java
@@ -1,4 +1,4 @@
-package edu.lu.uni.dataset.cnn;
+package edu.lu.uni.data.preparing;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,7 +21,8 @@ public class DataInitializer {
 
 	private static final String FEATURE_INTEGER_VECTOR_FILE_PATH = "dataset/integer-vectors/features/";
 	private static final String LABEL_INTEGER_VECTOR_FILE_PATH = "dataset/integer-vectors/labels/";
-	private static final String OUT_PUT_FILE_PATH = "src/main/resources/data-for-CNN/";
+	private static final String TRAINING_FILE_PATH = "src/main/resources/data-for-CNN/trainingdata/";
+	private static final String TESTING_FILE_PATH = "src/main/resources/data-for-CNN/testingdata/";
 	
 	public static void main(String[] args) throws IOException {
 		DataInitializer di = new DataInitializer();
@@ -73,6 +74,7 @@ public class DataInitializer {
 		
 		return labelClass;
 	}
+	
 
 	private void outputDataForCNNIntoFile(Map<String, Map<String, Integer>> labelsClassData,
 			Map<String, List<Map<String, List<String>>>> featuresData, Map<String, Integer> featuresMaxSizes) {
@@ -80,44 +82,41 @@ public class DataInitializer {
 		for (Map.Entry<String, List<Map<String, List<String>>>> featureData : featuresData.entrySet()) {
 			// each kind of features combines with each kind of label classes.
 			int maxSizeOfFeature = featuresMaxSizes.get(featureData.getKey());
+			String fileNameOfFeature = featureData.getKey();
+			fileNameOfFeature = fileNameOfFeature.substring(0, fileNameOfFeature.lastIndexOf(".list"));
 			List<Map<String, List<String>>> features = featureData.getValue();
 			
 			for (Map.Entry<String, Map<String, Integer>> labelData : labelsClassData.entrySet()) {
-				String fileName = featureData.getKey();
-				fileName = fileName.substring(0, fileName.lastIndexOf(".list"));
-				if (labelData.getKey().endsWith("(1).list")) {
-					fileName += "(1)";
-				} else if (labelData.getKey().endsWith("(2).list")) {
-					fileName += "(2)";
-				} else if (labelData.getKey().endsWith("(3).list")) {
-					fileName += "(3)";
-				}
-				fileName += "MaxSize=" + maxSizeOfFeature + ".csv";
-				
-				Map<String, Integer> labelsClass = labelData.getValue();
-				StringBuilder sb = new StringBuilder();
-				int numberOfData = 0;
-				for (Map<String, List<String>> feature : features) {
-					//combination of each feature
-					Object[] key = feature.keySet().toArray();
-					List<String> featureList = new ArrayList<String>();
-					featureList.addAll(feature.get(key[0]));
+				String fileName = labelData.getKey();
+				if (fileName.contains(fileNameOfFeature)) {
+					fileName = fileName.substring(0, fileName.lastIndexOf(".list"));
+					fileName += "MaxSize=" + maxSizeOfFeature + ".csv";
 					
-					appendZero(featureList, maxSizeOfFeature);
-					
-					sb.append(featureList.toString().replace("[", "").replace("]", "").replaceAll(" ", ""));
-					sb.append(",");
-					sb.append("" + labelsClass.get(key[0]));
-					sb.append("\n");
-					
-					numberOfData ++;
-					if (numberOfData % 1000 == 0) {
-						FileHelper.outputToFile(OUT_PUT_FILE_PATH + fileName, sb);
-						sb = new StringBuilder();
+					Map<String, Integer> labelsClass = labelData.getValue();
+					StringBuilder sb = new StringBuilder();
+					int numberOfData = 0;
+					for (Map<String, List<String>> feature : features) {
+						//combination of each feature
+						Object[] key = feature.keySet().toArray();
+						List<String> featureList = new ArrayList<String>();
+						featureList.addAll(feature.get(key[0]));
+						
+						appendZero(featureList, maxSizeOfFeature);
+						
+						sb.append(featureList.toString().replace("[", "").replace("]", "").replaceAll(" ", ""));
+						sb.append(",");
+						sb.append("" + labelsClass.get(key[0]));
+						sb.append("\n");
+						
+						numberOfData ++;
+						if (numberOfData % 1000 == 0) {
+							FileHelper.outputToFile(TRAINING_FILE_PATH + fileName, sb);
+							sb = new StringBuilder();
+						}
 					}
-				}
-				if (sb.length() > 0) {
-					FileHelper.outputToFile(OUT_PUT_FILE_PATH + fileName, sb);
+					if (sb.length() > 0) {
+						FileHelper.outputToFile(TESTING_FILE_PATH + fileName, sb);
+					}
 				}
 			}
 		}
---
+++ src/main/java/edu/lu/uni/deeplearning/VBNNPNNP/CNNon_feature_raw_tokens_without_operators.java
@@ -0,0 +1,130 @@
+package edu.lu.uni.deeplearning.VBNNPNNP;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CNNon_feature_raw_tokens_without_operators {
+	
+	/**
+	 * Number of label classes:
+	 * 197
+	 */
+	private static Logger log = LoggerFactory.getLogger(CNNon_feature_raw_tokens_without_operators.class);
+	private static final String TRAINING_DATA_FILE_PATH = "/data-for-CNN/trainingdata/";
+	private static final String TESTING_DATA_FILE_PATH = "/data-for-CNN/testingdata/";
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+		String fileName = "apache$commons-math$feature-raw-tokens-without-operators(SIMPLIFIED_NLP)(3)MaxSize=69.csv";
+		int indexOfMaxSize = fileName.lastIndexOf("MaxSize=");
+		int labelIndex = Integer.parseInt(fileName.substring(indexOfMaxSize + "MaxSize=".length(), fileName.lastIndexOf(".csv")));
+		int numClasses = 197;    
+		
+		int nChannels = 1;   // Number of input channels
+        int outputNum = numClasses; // The number of possible outcomes
+        int batchSize = 100; // Test batch size
+        int nEpochs = 1;     // Number of training epochs
+        int iterations = 1;  // Number of training iterations
+        int seed = 123;      //
+
+        log.info("Load data....");
+        RecordReader trainingDataReader = new CSVRecordReader();
+        trainingDataReader.initialize(new FileSplit(new ClassPathResource(TRAINING_DATA_FILE_PATH + fileName).getFile()));
+        DataSetIterator trainingData = new RecordReaderDataSetIterator(trainingDataReader,batchSize,labelIndex,numClasses);
+        RecordReader testingDataReader = new CSVRecordReader();
+        int testBatchSize = 821;
+        testingDataReader.initialize(new FileSplit(new ClassPathResource(TESTING_DATA_FILE_PATH + fileName).getFile()));
+        DataSetIterator testData = new RecordReaderDataSetIterator(testingDataReader,testBatchSize,labelIndex,numClasses);
+        
+        /*
+         *  Construct the neural network
+         */
+        log.info("Build model....");
+        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(iterations) // Training iterations as above
+                .regularization(true).l2(0.0005)
+                .learningRate(.01)//.biasLearningRate(0.02)
+                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
+                .weightInit(WeightInit.XAVIER)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(1, 5)
+                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
+                        .nIn(nChannels)
+                        .stride(1, 1)
+                        .nOut(20)
+                        .activation("identity")
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(1,2)
+                        .stride(1,2)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(1, 5)
+                        .stride(1, 1)
+                        .nOut(50)
+                        .activation("identity")
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(1,2)
+                        .stride(1,2)
+                        .build())
+                .layer(4, new DenseLayer.Builder().activation("relu")
+                        .nOut(500).build())
+                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.MEAN_ABSOLUTE_ERROR)
+                        .nOut(outputNum)
+                        .activation("softmax")
+                        .build())
+                .setInputType(InputType.convolutionalFlat(1,labelIndex,1))
+                .backprop(true).pretrain(false);
+
+        MultiLayerConfiguration conf = builder.build();
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+
+        log.info("Train model....");
+        model.setListeners(new ScoreIterationListener(1));
+        for( int i=0; i<nEpochs; i++ ) {
+            model.fit(trainingData);
+            log.info("*** Completed epoch {} ***", i);
+
+            log.info("Evaluate model....");
+            Evaluation eval = new Evaluation(outputNum);
+            while(testData.hasNext()){
+                DataSet ds = testData.next();
+                INDArray output = model.output(ds.getFeatureMatrix(), false);
+                eval.eval(ds.getLabels(), output);
+
+            }
+            log.info(eval.stats());
+            testData.reset();
+        }
+        log.info("****************Example finished********************");
+	}
+}
